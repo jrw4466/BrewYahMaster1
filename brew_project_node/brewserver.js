bda59cab6c3e6374520d49fad6e56c1b003c5f88
Jake Wyatt <Jake Wyatt>
2017-06-07 08:46:13 -0500

    server update tuesday class 060617

--- brew_project_node/brewserver.js
+++ brew_project_node/brewserver.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.options('*', cors());
 
 
-var BREW_API_URL = "https://api.brewerydb.com/v2/locations/?name&key=52840d61aed0d4d4dc14a975bf3092c4&format=json";
+var BREW_API_URL = "https://api.brewerydb.com/v2/search/geo/point/?lat=30.267&lng=-97.743&key=52840d61aed0d4d4dc14a975bf3092c4&format=json";
 
 
 app.get('/', function(req, res, next) {
