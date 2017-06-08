fcd59b53d21c74aec2d2266b3be19b7caca5d9f3
Samuel Jeon <samueljeon@Samuels-MacBook-Air.local>
2017-06-07 21:16:55 -0500

    clear files

--- brew_project_node/brewserver.js
+++ brew_project_node/brewserver.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.options('*', cors());
 
 
-var BREW_API_URL = "https://api.brewerydb.com/v2/locations/?name&key=52840d61aed0d4d4dc14a975bf3092c4&format=json";
+var BREW_API_URL = "https://api.brewerydb.com/v2/search/geo/point?lat=30.267&lng=-97.743&key=52840d61aed0d4d4dc14a975bf3092c4&format=json";
 
 
 app.get('/', function(req, res, next) {
@@ -17,7 +17,7 @@ app.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname + '/public/brew.html'))
 })
 
-app.get('/breweries', function(req, res, next) {
+app.get('/breweries/search/geo/point', function(req, res, next) {
     // https://stackoverflow.com/questions/8515872/simple-api-calls-with-node-js-and-express
     request(BREW_API_URL, function (error, response, body) {
         if (!error && response.statusCode == 200) {
